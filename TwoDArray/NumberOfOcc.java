21096be4deb963b67194892c74ba38efac520264
Fatima Arain <fatimaarain67@gmail.com>
2026-04-09 21:04:23 +0500

    twod Array

--- TwoDArray/NumberOfOcc.java
+++ TwoDArray/NumberOfOcc.java
@@ -1,24 +1,53 @@
 package If_Else.TwoDArray;
+/*
+the time complexity is O(M)square
+M=( r*c) square
+rows =3
+col =4
+3*4
 
+total no of op 120 approx
+O(M)square
+ */
 public class NumberOfOcc {
-    public static void numOfOcc(int[][] ar){
-        for (int i = 0; i <ar.length ; i++) {
-            int occ=0;
+    public static void numOfOcc(int[][] ar) {
+        boolean[][] visitArray = new boolean[ar.length][ar[0].length];
+        // this boolean array helps to print if a number is more then once time it will help to
+        //print once time
+        for (int i = 0; i < ar.length; i++) {
             for (int j = 0; j < ar[i].length; j++) {
-                for (int k = ar[i].length; k < j; k++) {
-                    if(ar[i][j] ==ar[j][k]){
-                        occ++;
-                    }
+                if (visitArray[i][j]) {
+                    i++;
+                    j++;
+                    continue;
                 }
-                if(occ>1){
-                    System.out.println(ar[i][j]+"is "+occ+" times");
+                int occ = 0;
+                for (int x = 0; x < ar.length; x++) {
+                    for (int y = 0; y < ar[x].length; y++) {
 
+                        if (ar[i][j] == ar[x][y])
+                        //!visitArray[i][j] = visitArray[i][j]==false
+                        {
+                            visitArray[x][y] = true;
+                            occ++;
+                        }
+                        //end if
+                    } //end y loop
+                } //end x loop
+
+
+                if (occ > 1)// this is visitArray == true
+                {
+//
+//                }
+
+                    System.out.println(ar[i][j] + " " + occ + " times");
                 }
 
             }
         }
-    }
 
+    }
     static void main() {
         int [][] ar={
                 {1,2,3,7},{4,5,6,7},{8,9,7,6}
