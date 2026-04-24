ba855416faf6e4bbc180b4998b96d0829bdab09f
Fatima <fatimaarain67@gmail.com>
2026-04-24 09:13:52 +0500

    updating task Array

---
+++ ArrayDSA/Task/PrintReverseArrayUsingRec.java
@@ -0,0 +1,19 @@
+package If_Else.ArrayDSA.Task;
+
+public class PrintReverseArrayUsingRec {
+    public static void rec(int[] a,int i)
+    {
+        if(i == a.length-1) return;
+i++;
+        rec(a,i);
+
+        System.out.print(a[i]+" ");
+
+    }
+
+    static void main() {
+        int [] a = new int[4];
+        a[0]=901; a[1]=902; a[2]=903; a[3]=904;
+        rec(a,-1);
+    }
+}
