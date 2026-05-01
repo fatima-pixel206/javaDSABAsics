d6913e615cec6d8bd580a6595fd8a6f662f6c43d
Fatima <fatimaarain67@gmail.com>
2026-05-01 16:04:21 +0500

    arrays

--- ArrayDSA/MaxMin.java
+++ ArrayDSA/MaxMin.java
@@ -2,12 +2,12 @@ package If_Else.ArrayDSA;
 
 public class MaxMin {
     static void main(String[] args) {
-        int[]  ar = {5,-90};
+        int[]  ar = {5,-90,56};
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < ar.length; i++) {
             if(ar[i] < min)
-                min= ar[i];
+                min = ar[i];
             if(ar[i]> max)
                 max = ar[i];
         }
